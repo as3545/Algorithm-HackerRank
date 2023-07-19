2b65f91a78f63ae087d39399df41776fcc44f7ba
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-19 14:44:04 +0530

    Solve Me First sum of two.cpp

---
+++ Solve Me First sum of two.cpp
@@ -0,0 +1,50 @@
+/*
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+
+
+Return .
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of  and 
+
+Constraints
+
+
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+
+.
+*/
+
+//Code
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b;
+    cin>>a >>b;
+    cout<<a+b<<endl;  
+    return 0;
+}
